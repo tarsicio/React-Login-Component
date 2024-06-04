53bd195e4b47424f4c102ab316c8dcd8687b2800
developer <cquevedo@developer.local>
2024-06-04 09:43:03 -0400

    Cambios en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { path } from 'path';
-
-const __dirname = path.resolve(path.dirname(''));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@components': path.resolve(__dirname, './src/components'), // Alias para la carpeta components
-    },
-  },
 })
